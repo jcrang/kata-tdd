e31a4f875ecb215c236fee109e9bf9e67d701b77
Josh Crang <josh.crang@red-gate.com>
2017-06-02 14:14:21 +0100

    Sorting empty set of cards

---
+++ Exercise8/Exercise8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Exercise8/Exercise8/Tests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace Exercise8
+{
+    public class Tests
+    {
+        [Test]
+        public void SortCards_NoCards_IsEmpty()
+        {
+            var sortedCards = CardSorter.SortCards(Enumerable.Empty<Card>());
+
+            Assert.That(sortedCards, Is.Empty);
+        }
+    }
+
+    public class Card
+    {
+    }
+
+    public static class CardSorter
+    {
+        public static IEnumerable<Card> SortCards(IEnumerable<Card> empty)
+        {
+            return Enumerable.Empty<Card>();
+        }
+    }
+}
