17af11532aa7af099d4140c617607e4b6eb5b66a
Josh Crang <josh.crang@red-gate.com>
2023-05-12 13:04:53 +0100

    Initial commit with tests

---
+++ InterviewRota/InterviewRota/InterviewRotaTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace InterviewRota;
+
+[TestFixture]
+public class InterviewRotaTests
+{
+    [Test]
+    public void OneAddOne()
+    {
+        Assert.That(1 + 1, Is.EqualTo(2));
+    }
+}
